5b375051f0519d810d5d999e9d8f1cbe2cd1b82d
Jose Baez <jlb406@gmail.com>
2015-05-16 16:21:29 -0400

    Created intial code for searching

---
+++ search.go
@@ -0,0 +1,61 @@
+package itunessearch
+
+import (
+    "encoding/json"
+    "io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var itunesSearchURl = "https://itunes.apple.com/search"
+
+type SearchResponse struct {
+	ResultCount int `bson:"resultCount"`
+	Results []SearchResult `bson:"results"`
+}
+
+type SearchResult struct {
+	ArtworkUrl30 string `json:"artworkUrl30" bson:"artworkUrl30"`
+	ArtworkUrl60 string `json:"artworkUrl60" bson:"artworkUrl60"`
+	ArtworkUrl100 string `json:"artworkUrl100" bson:"artworkUrl100"`
+	ArtistName string `json:"artistName" bson:"artistName"`
+	CollectionName string `json:"collectionName" bson:"collectionName"`
+	CollectionId int `json:"collectionId" bson:"collectionId"`
+	FeedUrl string `json:"feedUrl" bson:"feedUrl"`
+}
+
+func Search(query string, mediaType string, limit uint) []SearchResult {
+	baseUrl, err := url.Parse(itunesSearchURl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	params := url.Values{}
+	params.Add("term", query)
+	params.Add("media", mediaType)
+	params.Add("limit", string(limit))
+
+	baseUrl.RawQuery = params.Encode()
+
+	log.Printf("search url: %s", baseUrl)
+
+	resp, err := http.Get(baseUrl.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var response SearchResponse
+
+	err = json.Unmarshal([]byte(body), &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return response.Results;
+}
